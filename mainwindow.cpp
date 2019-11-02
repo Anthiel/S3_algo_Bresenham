f18af3a40c31262ff8fdf1dad0d12bae82b6fec0
Antoine <Antoine.thiel83@gmail.com>
2019-11-02 19:16:17 +0100

    dessiner une droite avec deux points

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,59 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->plot->xAxis->setLabel("x");
+    ui->plot->yAxis->setLabel("y");
+    ui->plot->xAxis->setRange(0, 10);
+    ui->plot->yAxis->setRange(0, 10);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::drawLineOnPlot(double x1, double y1, double x2, double y2){
+
+    QVector<double> X{x1,x2};
+    QVector<double> Y{y1,y2};
+
+    ui->plot->addGraph();
+    ui->plot->graph(0)->setData(X,Y);
+    ui->plot->replot();
+}
+
+
+/* SLOT de Qt */
+
+void MainWindow::on_spinBox_valueChanged(const QString &arg1)
+{
+    x1 = arg1.toInt();
+}
+
+void MainWindow::on_spinBox_2_valueChanged(const QString &arg1)
+{
+    y1 = arg1.toInt();
+}
+
+void MainWindow::on_spinBox_3_valueChanged(const QString &arg1)
+{
+    x2 = arg1.toInt();
+}
+
+void MainWindow::on_spinBox_4_valueChanged(const QString &arg1)
+{
+    y2 = arg1.toInt();
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    drawLineOnPlot(x1, y1, x2, y2);
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <iostream>
+#include <QVector>
 
 namespace Ui {
 class MainWindow;
@@ -14,6 +16,24 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+    void drawLineOnPlot(double x1, double y1, double x2, double y2);
+
+    int x1 = 0, y1 = 0; // premier point
+    int x2 = 0, y2 = 0; // deuxieme point
+
+
+
+private slots:
+    void on_spinBox_valueChanged(const QString &arg1);
+
+    void on_spinBox_2_valueChanged(const QString &arg1);
+
+    void on_spinBox_3_valueChanged(const QString &arg1);
+
+    void on_spinBox_4_valueChanged(const QString &arg1);
+
+    void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
